1474a924bf827d0e243e08b4aa37ce6e167db2b8
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2020-06-08 02:57:18 -0300

    ✨ (graphql): setup Apollo with Express

--- packages/graphql/src/main.ts
+++ packages/graphql/src/main.ts
@@ -1,9 +1,38 @@
-import * as fs from 'fs';
+import pkg from '../package.json';
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 
-fs.readdir(__dirname, (error, files) => {
-  if (error) {
-    console.error(error.message);
-    return;
-  }
-  console.log(files);
+const app = express();
+
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Query {
+      version: String!
+    }
+  `,
+  resolvers: {
+    Query: {
+      version: () => pkg.version,
+    },
+  },
+});
+
+server.applyMiddleware({
+  app,
+  cors: {
+    origin: [
+      'http://localhost:3001',
+      'https://localhost:3001',
+      'http://127.0.0.1:3001',
+      'https://127.0.0.1:3001',
+    ],
+  },
+  bodyParserConfig: true,
+});
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
+
+app.listen(PORT, HOSTNAME, () => {
+  console.log(`Server is listening at http://${HOSTNAME}:${PORT}.`);
 });
